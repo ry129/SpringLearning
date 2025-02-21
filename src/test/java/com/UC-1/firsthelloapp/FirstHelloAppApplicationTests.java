d9a70023e3a71315401e8808aaa790e3edea3a1f
ry129 <0111cs211146.riya@gmail.com>
2025-02-21 19:08:54 +0530

    [Riya Yadav]Add. UC1 basic hello app using Spring boot

---
+++ src/test/java/com/UC-1/firsthelloapp/FirstHelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firsthelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHelloAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
